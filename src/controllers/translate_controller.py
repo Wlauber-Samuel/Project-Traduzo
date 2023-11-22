2b0048e1176d1df0675dfffaded744eedfbb209e
Wlauber <wlaubersmuel@gmail.com>
2023-11-22 15:50:45 -0300

    requisito 06 concluído

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from src.models.language_model import LanguageModel
+from deep_translator import GoogleTranslator
 
 
 translate_controller = Blueprint("translate_controller", __name__)
@@ -14,4 +15,24 @@ def index():
             translate_from="pt",
             translate_to="en",
             translated="What do you want to translate?"
+    )
+
+
+@translate_controller.route("/", methods=["POST"])
+def translate():
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+
+    translated = GoogleTranslator(
+        source=translate_from, target=translate_to
+        ).translate(text_to_translate)
+
+    return render_template(
+            "index.html",
+            languages=LanguageModel.list_dicts(),
+            text_to_translate=text_to_translate,
+            translate_from=translate_from,
+            translate_to=translate_to,
+            translated=translated
     )
